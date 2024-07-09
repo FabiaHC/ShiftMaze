e0d78fa27d1ab4cb3d203d32a4cbc2794e9cce1b
Fabian <megaweepinbell@gmail.com>
2024-07-09 17:27:54 +0200

    Add scene system and main menu

---
+++ src/SceneSystem.py
@@ -0,0 +1,26 @@
+class Scene:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def handleEvents(self):
+        pass
+
+    def draw(self, screen):
+        pass
+
+
+class MainMenu(Scene):
+    def __init__(self):
+        super().__init__()
+
+    def update(self):
+        pass
+
+    def handleEvents(self):
+        pass
+
+    def draw(self):
+        pass
--- src/ShiftMaze.py
+++ src/ShiftMaze.py
@@ -1,5 +1,30 @@
+from SceneSystem import MainMenu
+
+import pygame
+
+
 def main():
-    print("Hello World!")
+    pygame.init()
+    screen = pygame.display.set_mode((800, 600))
+    clock = pygame.time.Clock()
+
+    currentScene = MainMenu()
+    running = True
+
+    while running:
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                running = False
+
+        # update scene
+        currentScene.handleEvents()
+        currentScene.update()
+        currentScene.draw()
+        pygame.display.flip()
+        clock.tick(60)
+
+    pygame.quit()
 
 
 if __name__ == "__main__":
