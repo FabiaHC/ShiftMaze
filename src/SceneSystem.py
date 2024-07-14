d8da7e3c2bb95eed4fa715744f0cda3087739ba5
Fabian <megaweepinbell@gmail.com>
2024-07-14 19:47:44 +0200

    add sliding animation

--- src/SceneSystem.py
+++ src/SceneSystem.py
@@ -50,8 +50,32 @@ class GamePlay(Scene):
         self.tiles = tileGenerator()
         self.selectedRow = 0
 
+        self.slideOffset = 0
+        self.slideDirection = 1
+        self.slidingRow = None
+        self.slidingCol = None
+
     def update(self):
-        pass
+        if self.slideOffset != 0:
+            self.slideOffset = abs(self.slideOffset)
+            self.slideOffset += 1
+            self.slideOffset %= 96
+            self.slideOffset *= self.slideDirection
+            if self.slideOffset == 0:
+
+                if self.slidingRow != None:
+                    if self.slideDirection == 1:
+                        shiftRow(self.tileBoard, self.slidingRow, False)
+                    elif self.slideDirection == -1:
+                        shiftRow(self.tileBoard, self.slidingRow, True)
+                    self.slidingRow = None
+
+                elif self.slidingCol != None:
+                    if self.slideDirection == 1:
+                        shiftColumn(self.tileBoard, self.slidingCol, True)
+                    elif self.slideDirection == -1:
+                        shiftColumn(self.tileBoard, self.slidingCol, False)
+                    self.slidingCol = None
 
     def handleEvents(self, events):
         for event in events:
@@ -61,18 +85,31 @@ class GamePlay(Scene):
                 SBSS = 3*16 #shift button shorter size
                 x -= 160    #remove offset
                 y -= 60     #remove offset
+
+                if self.slideOffset != 0:   #ignore input during sliding animation
+                    continue
+
                 if y > 0 and y < 5*96:
                     row = y // 96
-                    if x > -SBSS and x < 0:
-                        shiftRow(self.tileBoard, row, False)
-                    elif x > 5*96 and x < 5*96+SBSS:
-                        shiftRow(self.tileBoard, row, True)
+                    if x > -SBSS and x < 0:             #right shift arrows
+                        self.slidingRow = row
+                        self.slideOffset = 1
+                        self.slideDirection = 1
+                    elif x > 5*96 and x < 5*96+SBSS:    #left shift arrows
+                        self.slidingRow = row
+                        self.slideOffset = -1
+                        self.slideDirection = -1
+
                 if x > 0 and x < 5*96:
                     column = x // 96
-                    if y > -SBSS and y < 0:
-                        shiftColumn(self.tileBoard, column, True)
-                    elif y > 5*96 and y < 5*96+SBSS:
-                        shiftColumn(self.tileBoard, column, False)
+                    if y > -SBSS and y < 0:             #down shift arrows
+                        self.slidingCol = column
+                        self.slideOffset = 1
+                        self.slideDirection = 1
+                    elif y > 5*96 and y < 5*96+SBSS:    #up shift arrows
+                        self.slidingCol = column
+                        self.slideOffset = -1
+                        self.slideDirection = -1
 
     def draw(self, screen):
         screen.fill(config.green3)
@@ -80,7 +117,13 @@ class GamePlay(Scene):
         yOffset = 60
         for y in range(5):
             for x in range(5):
-                screen.blit(self.tiles[self.tileBoard[y][x]], (xOffset+x*96, yOffset+y*96))
+                if x == self.slidingCol:
+                    screen.blit(self.tiles[self.tileBoard[y][x]], (xOffset+x*96, yOffset+y*96 + self.slideOffset))
+                elif y == self.slidingRow:
+                    screen.blit(self.tiles[self.tileBoard[y][x]], (xOffset+x*96+self.slideOffset, yOffset+y*96))
+                else:
+                    screen.blit(self.tiles[self.tileBoard[y][x]], (xOffset+x*96, yOffset+y*96))
+
 
         for y in range(5):
             screen.blit(self.tiles[6], (xOffset-3*16, yOffset+y*6*16))
